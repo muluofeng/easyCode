0a725bad39aa3121645ea666f7f0e09a8c67d302
muluofeng <906877230@qq.com>
2024-10-14 15:23:35 +0800

    反射调用 多数据源

--- easy-code-springboot-starter/src/main/java/com/muluofeng/easycode/core/service/SysGeneratorService.java
+++ easy-code-springboot-starter/src/main/java/com/muluofeng/easycode/core/service/SysGeneratorService.java
@@ -1,18 +1,21 @@
 package com.muluofeng.easycode.core.service;
 
-import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
-import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
+import cn.hutool.core.collection.CollUtil;
 import com.muluofeng.easycode.core.dao.SysGeneratorDao;
 import com.muluofeng.easycode.core.dto.DataSourceDTO;
 import com.muluofeng.easycode.core.dto.GeneratorCodeDTO;
 import com.muluofeng.easycode.core.dto.SearchTableDTO;
 import com.muluofeng.easycode.core.utils.GenUtils;
-import com.zaxxer.hikari.HikariDataSource;
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -28,45 +31,58 @@ public class SysGeneratorService {
     private SysGeneratorDao sysGeneratorDao;
     @Resource
     private DataSource dataSource;
-   public static final  String masterSourceKey = "master";
+    public static final String masterSourceKey = "master";
+
+    private  List<DataSourceDTO> dynamicDataSources = new ArrayList<>();
+    private  List<String> dynamicDataSourceKeys = new ArrayList<>();
+
+    @PostConstruct
+    public void initDataSource() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String dataSourceName = dataSource.getClass().getName();
+
+        if (dataSourceName.equals("com.baomidou.dynamic.datasource.DynamicRoutingDataSource")) {
+            // 使用反射加载 DynamicRoutingDataSource 类
+            Class<?> dynamicRoutingDataSourceClass = Class.forName("com.baomidou.dynamic.datasource.DynamicRoutingDataSource");
+            if (dynamicRoutingDataSourceClass.isInstance(dataSource)) {
+                // 强制转换为 DynamicRoutingDataSource，并获取数据源
+                Object dynamicRoutingDataSource = dynamicRoutingDataSourceClass.cast(dataSource);
+                Method getDataSourcesMethod = dynamicRoutingDataSourceClass.getMethod("getDataSources");
+                Map<String, DataSource> dataSources = (Map<String, DataSource>) getDataSourcesMethod.invoke(dynamicRoutingDataSource);
+                dynamicDataSources = dataSources.keySet().stream().map(key ->
+                        DataSourceDTO.builder().sourceKey(key).sourceName(key +"数据源").build()).toList();
+                 dynamicDataSourceKeys = dynamicDataSources.stream().map(DataSourceDTO::getSourceKey).toList();
+            }
+        }
+    }
 
     public List<Map<String, Object>> queryList(SearchTableDTO req) {
         String dataSourceKey = req.getDataSource();
-        if (!masterSourceKey.equals(dataSourceKey)) {
-            DynamicDataSourceContextHolder.push(dataSourceKey);
-        }
+        changeDataSource(dataSourceKey);
         List<Map<String, Object>> res = sysGeneratorDao.queryList(req);
-        DynamicDataSourceContextHolder.poll();
+        changeAfterDataSource(dataSourceKey);
         return res;
     }
-
 
 
     public int queryTotal(SearchTableDTO req) {
         String dataSourceKey = req.getDataSource();
-        if (!masterSourceKey.equals(dataSourceKey)) {
-            DynamicDataSourceContextHolder.push(dataSourceKey);
-        }
+        changeDataSource(dataSourceKey);
         int res = sysGeneratorDao.queryTotal(req);
-        DynamicDataSourceContextHolder.poll();
+        changeAfterDataSource(dataSourceKey);
         return res;
     }
 
     public Map<String, String> queryTable(String tableName, String dataSourceKey) {
-        if (!masterSourceKey.equals(dataSourceKey)) {
-            DynamicDataSourceContextHolder.push(dataSourceKey);
-        }
+        changeDataSource(dataSourceKey);
         Map<String, String> res = sysGeneratorDao.queryTable(tableName);
-        DynamicDataSourceContextHolder.poll();
+        changeAfterDataSource(dataSourceKey);
         return res;
     }
 
     public List<Map<String, String>> queryColumns(String tableName, String dataSourceKey) {
-        if(!masterSourceKey.equals(dataSourceKey)){
-            DynamicDataSourceContextHolder.push(dataSourceKey);
-        }
+        changeDataSource(dataSourceKey);
         List<Map<String, String>> res = sysGeneratorDao.queryColumns(tableName);
-        DynamicDataSourceContextHolder.poll();
+        changeAfterDataSource(dataSourceKey);
         return res;
     }
 
@@ -101,6 +117,7 @@ public class SysGeneratorService {
 
     /**
      * 生成代码到目录
+     *
      * @param req
      */
     public void codeToDirect(GeneratorCodeDTO req) {
@@ -108,26 +125,40 @@ public class SysGeneratorService {
         String dataSourceKey = req.getDataSource();
         for (String tableName : tableNames) {
             // 查询表信息
-            Map<String, String> table = queryTable(tableName,dataSourceKey);
+            Map<String, String> table = queryTable(tableName, dataSourceKey);
             // 查询列信息
-            List<Map<String, String>> columns = queryColumns(tableName,dataSourceKey);
+            List<Map<String, String>> columns = queryColumns(tableName, dataSourceKey);
             // 生成代码
-            GenUtils.generatorCodeToDirect(easyCodePropertiesConfiguration,table, columns);
+            GenUtils.generatorCodeToDirect(easyCodePropertiesConfiguration, table, columns);
         }
 
     }
 
     public List<DataSourceDTO> getDataSources() {
-        if(dataSource instanceof HikariDataSource){
-            DataSourceDTO defaultSource = DataSourceDTO.builder().sourceName("默认数据数据源").sourceKey(masterSourceKey).build();
-            return List.of(defaultSource);
-        }else if(dataSource instanceof  com.baomidou.dynamic.datasource.DynamicRoutingDataSource ){
-            DynamicRoutingDataSource dynamicRoutingDataSource = (DynamicRoutingDataSource) dataSource;
-            Map<String, DataSource> dataSources = dynamicRoutingDataSource.getDataSources();
-            return  dataSources.entrySet().stream().map(
-                    entry-> DataSourceDTO.builder().sourceName(entry.getKey()+"数据源").sourceKey(entry.getKey()).build()
-            ).toList();
+        if(CollUtil.isNotEmpty(dynamicDataSources)){
+            return dynamicDataSources;
         }
-        return List.of();
+        DataSourceDTO defaultSource = DataSourceDTO.builder().sourceName("默认数据数据源").sourceKey(masterSourceKey).build();
+        return List.of(defaultSource);
+    }
+
+    @SneakyThrows
+    public void changeDataSource(String dataSourceKey) {
+        if(CollUtil.isNotEmpty(dynamicDataSources) && dynamicDataSourceKeys.contains(dataSourceKey)) {
+            Class<?> dynamicRoutingDataSource = Class.forName("com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder");
+
+            Method method = dynamicRoutingDataSource.getDeclaredMethod("push", String.class);
+            method.invoke(null,dataSourceKey);
+        }
+    }
+
+    @SneakyThrows
+    public void changeAfterDataSource(String dataSourceKey) {
+        if(CollUtil.isNotEmpty(dynamicDataSources) && dynamicDataSourceKeys.contains(dataSourceKey)) {
+            Class<?> dynamicRoutingDataSource = Class.forName("com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder");
+            Method method = dynamicRoutingDataSource.getDeclaredMethod("poll");
+            method.invoke(null);
+
+        }
     }
 }
